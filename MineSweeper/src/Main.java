783e1bb1be99fc82ca67dadd07eda2b23ece0988
Albert Morales <albert.morales@upr.edu>
2016-10-07 00:13:47 -0400

    Labels and Timer

--- MineSweeper/src/Main.java
+++ MineSweeper/src/Main.java
@@ -1,4 +1,8 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class Main {
 	public static void main(String[] args) {
@@ -8,13 +12,36 @@ public class Main {
 		myFrame.setSize(600, 400);
 
 		MyPanel myPanel = new MyPanel();
-		myFrame.add(myPanel);
-
+		myFrame.getContentPane().add(myPanel);
+		myPanel.setLayout(null);
+		
+		JLabel lblFlags = new JLabel("Flags: ");
+		lblFlags.setBounds(28, 8, 38, 15);
+		lblFlags.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblFlags.setHorizontalAlignment(SwingConstants.LEFT);
+		myPanel.add(lblFlags);
+		
+		JButton btnReset = new JButton("RESET");
+		btnReset.setBounds(113, 0, 94, 23);
+		btnReset.setFont(new Font("Tahoma", Font.BOLD, 12));
+		myPanel.add(btnReset);
+		
+		
+		JLabel lblTime = new JLabel("Time: " + myPanel.getTime()  + " seconds");
+		lblTime.setBounds(235, 9, 174, 15);
+		lblTime.setHorizontalAlignment(SwingConstants.LEFT);
+		lblTime.setFont(new Font("Tahoma", Font.BOLD, 12));
+		myPanel.add(lblTime);
+		
+		
+		
+		
+		
+		
 		MyMouseAdapter myMouseAdapter = new MyMouseAdapter();
 		myFrame.addMouseListener(myMouseAdapter);
-
-		
 		
 		myFrame.setVisible(true);
 	}
+
 }
